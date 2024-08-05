eb773f9d5338ca6ddbf9a633bf6d48c958d55733
RAGHUDATHESH G P <datheshraghu@gmail.com>
2024-08-05 16:49:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ def amazon():
 
 
 if __name__=="__main__":
-    app.run(debug=True, port=5000)
-    #app.run(debug=True, port=5000,host="0.0.0.0")
+    #app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000,host="0.0.0.0")
